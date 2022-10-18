780170f4abcaf61a35dc97282116f4006adc57fa
Robert Greener <me@r0bert.dev>
2022-10-18 10:18:58 +0000

    Add beliefs stub
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- generate-config.py
+++ generate-config.py
@@ -15,3 +15,25 @@ df = pl.DataFrame({
 })
 
 df.write_json("output/behaviours.json", row_oriented=True)
+
+# Process beliefs -------------------------------------------------------------
+
+belief_namespace = UUID("034d3135-f1f0-441b-a476-9fac37cafc92")
+
+beliefs = [
+    "I care about the environment",
+    "I want to get to work quickly",
+    "I care about the social importance of the car",
+    "I want to keep fit",
+    "I do not want to perform exercise on my commute",
+    "Cycling is hard work",
+    "I'm not fit enough to walk",
+    "I don't think cycling is cool / fun",
+    "Car driving is more convenient",
+    "I'm scared of getting hit by a car",
+    "My bike might get stolen",
+    "Cycling is dangerous",
+    "I get to see the environment when I cycle",
+    "Walking allows me to experience the environment",
+    "I feel unsafe walking"
+]
