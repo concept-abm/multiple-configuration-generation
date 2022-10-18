b1f2da9d7f3584c3fbf476eb8443d39caf8e4803
Robert Greener <me@r0bert.dev>
2022-10-18 16:14:59 +0000

    Try to use AWS
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- generate-config.py
+++ generate-config.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python
 
 import polars as pl
+import os
 import numpy as np
 import pandas as pd
+import boto3
+import logging
+from botocore.exceptions import ClientError
 from uuid import uuid5, UUID
 from scipy.stats import bernoulli, truncnorm
+
+# s3 --------------------------------------------------------------------------
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 
 # Configuration ---------------------------------------------------------------
 
 n_scenarios = 10
+
 # Process behaviours ----------------------------------------------------------
 
 behaviours = ["Walk", "Cycle", "PT", "Drive"]
@@ -21,6 +52,9 @@ behaviour_df = pl.DataFrame({
 })
 
 behaviour_df.write_json("output/behaviours.json", row_oriented=True)
+
+upload_file("output/behaviours.json", "concept-abm",
+            object_name="behaviours.json")
 
 # Process beliefs -------------------------------------------------------------
 
